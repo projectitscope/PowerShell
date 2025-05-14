f6c1bdc1d315b23b60ec22a18c1174664f525fde
IT Scope LTDA <44481678+projectitscope@users.noreply.github.com>
2025-05-14 15:29:28 -0300

    Create No_admin.bat
    instrução que retira determinado usuario de determinada máquina.

---
+++ No_admin.bat
@@ -0,0 +1,4 @@
+::if "%COMPUTERNAME%"=::="NTBDEL012" (
+        :: Remove o usuário "pedro.paulo" do grupo "Teste"
+::        net localgroup Teste "DM-SUPERLOPES\pedro.paulo" /delete)
+echo "Teste F" > C:\Intel\TesteF.txt
